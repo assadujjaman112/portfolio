8842714464d37792198231fbc9103468425d9ed8
Assadujjaman Murad <assadujjamanmurad120@gmail.com>
2025-02-21 22:53:36 +0600

    edited the project section

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,16 +1,36 @@
-import { MdLocalPhone, MdEmail, MdLocationOn  } from "react-icons/md";
+import { MdLocalPhone, MdEmail, MdLocationOn } from "react-icons/md";
 
 const Contact = () => {
-    return (
-        <div className="text-white my-5 md:my-8 lg:my-12 w-11/12 md:w-4/5 mx-auto">
-            <h1 className="text-center text-3xl md:text-4xl lg:text-4xl font-bold">Contact</h1>
-            <div className="bg-slate-200 rounded-lg py-5 text-black flex flex-col pl-5 md:pl-10 lg:pl-0 lg:flex-row justify-center lg:justify-evenly mt-5">
-                <h1 className="flex items-center gap-2"> <span className="text-lg font-bold flex gap-2 items-center"><MdEmail className="text-xl"></MdEmail> Email </span><span className="font-bold">:</span> assadujjamanmurad120@gamil.com</h1>
-                <h1 className="flex items-center gap-2"> <span className="text-lg font-bold flex gap-2 items-center"><MdLocalPhone className="text-xl"></MdLocalPhone> Phone : </span>+8801959691545</h1>
-                <h1 className="flex items-center gap-2"> <span className="text-lg font-bold flex gap-2 items-center"><MdLocationOn className="text-xl"></MdLocationOn> Address : </span>Dhanmondi, Dhaka</h1>
-            </div>
-        </div>
-    );
+  return (
+    <div className="text-white my-5 md:my-8 lg:my-12 w-11/12 md:w-4/5 mx-auto">
+      <h1 className="text-center text-3xl md:text-4xl lg:text-4xl font-bold">
+        Contact
+      </h1>
+      <div className="bg-slate-200 rounded-lg py-5 text-black flex flex-col pl-5 md:pl-10 lg:pl-0 lg:flex-row justify-center lg:justify-evenly mt-5">
+        <h1 className="flex items-center gap-2">
+          {" "}
+          <span className="text-lg font-bold flex gap-2 items-center">
+            <MdEmail className="text-xl"></MdEmail> Email{" "}
+          </span>
+          <span className="font-semibold">:</span>assadujjamanmurad120@gamil.com
+        </h1>
+        <h1 className="flex items-center gap-2">
+          {" "}
+          <span className="text-lg font-bold flex gap-2 items-center">
+            <MdLocalPhone className="text-xl"></MdLocalPhone> Phone :{" "}
+          </span>
+          +8801959691545
+        </h1>
+        <h1 className="flex items-center gap-2">
+          {" "}
+          <span className="text-lg font-bold flex gap-2 items-center">
+            <MdLocationOn className="text-xl"></MdLocationOn> Address :{" "}
+          </span>
+          Dhanmondi, Dhaka
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,70 +1,82 @@
 const Projects = () => {
-  const img = "https://i.postimg.cc/Wpr0kv5n/Screenshot-2023-12-11-181448.png";
+  const projects = [
+    {
+      name: "Bermiz",
+      technologies: [
+        "JavaScript",
+        "React.js",
+        "Node.js",
+        "Express.js",
+        "MongoDB",
+      ],
+      link: "https://multi-perpuse-restaurant.web.app",
+      image: "https://i.postimg.cc/0NMyZbg8/bermiz.png",
+    },
+    {
+      name: "GadgetGrove",
+      technologies: [
+        "JavaScript",
+        "React.js",
+        "Node.js",
+        "Express.js",
+        "MongoDB",
+        "Firebase",
+      ],
+      link: "https://brand-shop-d7a8b.web.app",
+      image: "https://i.postimg.cc/2jF80stB/gadget-Grove.png",
+    },
+    {
+      name: "iApplyNow",
+      technologies: [
+        "JavaScript",
+        "React.js",
+        "Node.js",
+        "Express.js",
+        "MongoDB",
+        "Firebase",
+      ],
+      link: "https://job-seeking-a86ff.web.app",
+      image: "https://i.postimg.cc/NfsGSNhC/job-Seeking.png",
+    },
+  ];
   return (
     <div>
       <h1 className="text-white font-bold text-3xl md:text-4xl lg:text-5xl text-center my-5">
         My Projects
       </h1>
-      <section className="w-11/12 md:w-4/5 mx-auto my-5 md:my-8 lg:my-12">
-        <div className="carousel">
-          <div id="slide1" className="carousel-item relative w-full h-[500px]">
-            <div className="relative w-full">
-              <img src={img} className="w-full h-[500px]" />
-              <a
-                className="text-red-800 text-2xl font-extrabold absolute -mt-10 ml-5 text-center w-full"
-                href="https://job-seeking-a86ff.web.app"
-              >
-                Project Live Link
-              </a>
+      <section className="w-11/12 md:w-4/5 mx-auto my-5 md:my-8 lg:my-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {projects?.map((project, i) => (
+          <a href={project.link} key={i}>
+            <div className="bg-slate-800 px-3 pt-3 pb-10 rounded-lg">
+              <img
+                src={project.image}
+                alt=""
+                className="w-full h-[400px] object-right-top rounded-t-lg"
+              />
+              <div className="mt-4 flex justify-between items-center">
+                <h4 className="text-white text-3xl font-semibold mb-3">
+                  {project.name}
+                </h4>
+                <a
+                  href={project.link}
+                  className="text-sm font-bold text-blue-400 hover:underline"
+                >
+                  Project Live Link
+                </a>
+              </div>
+              <div className="flex flex-wrap justify-center gap-2 mt-6">
+                {project?.technologies?.map((tech, i) => (
+                  <div
+                    key={i}
+                    className="text-sm bg-blue-200 font-semibold px-3 py-1 rounded-lg"
+                  >
+                    {tech}
+                  </div>
+                ))}
+              </div>
             </div>
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide3" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide2" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-          <div id="slide2" className="carousel-item relative w-full">
-          <div className="relative w-full">
-              <img src= "https://i.postimg.cc/K4XYFjKH/Screenshot-2023-12-11-185157.png" className="w-full h-[500px]" />
-              <a
-                className="text-red-800 text-2xl font-extrabold absolute -mt-10 ml-5 text-center w-full"
-                href="https://event-management-assignm-7f14b.firebaseapp.com/"
-              >
-                Project Live Link
-              </a>
-            </div>
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide1" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide3" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-          <div id="slide3" className="carousel-item relative w-full">
-          <div className="relative w-full">
-              <img src= "https://i.postimg.cc/zvNJPM4s/Screenshot-2023-12-11-190101.png" className="w-full h-[500px]" />
-              <a
-                className="text-red-800 text-2xl font-extrabold absolute -mt-10 ml-5 text-center w-full"
-                href="https://brand-shop-d7a8b.firebaseapp.com"
-              >
-                Project Live Link
-              </a>
-            </div>
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide2" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide1" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-        </div>
+          </a>
+        ))}
       </section>
     </div>
   );
